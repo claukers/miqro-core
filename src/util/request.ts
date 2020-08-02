067b7c71bfcf81ea58960ece3482e30297cae4e0
claukers <claukers@amelia>
2020-08-01 23:47:28 -0400

    request module refactor and fix posible throw in JSON.parse in EndListener

--- src/util/request.ts
+++ src/util/request.ts
@@ -1,6 +1,6 @@
 import {Logger} from "./logger";
 import {ClientRequest, IncomingHttpHeaders, IncomingMessage, OutgoingHttpHeaders, request as httpRequest} from "http";
-import {format as formatUrl, parse as urlParse} from "url";
+import {format as formatUrl, parse as urlParse, UrlWithStringQuery} from "url";
 import {request as httpsRequest} from "https";
 import {NamedError} from "./error/named";
 import {Util} from "./util";
@@ -38,6 +38,109 @@ export interface RequestResponse {
   response: IncomingMessage;
   data: any;
   request: ClientRequest;
+}
+
+const requestCallback = (url: UrlWithStringQuery, options: RequestOptions, request: ClientRequest, logger: Logger) => (res: IncomingMessage): Promise<RequestResponse> => {
+  return new Promise<RequestResponse>((resolve, reject) => {
+    let data: any = "";
+    const chunkListener = (chunk: any) => {
+      data += chunk;
+    };
+    const errorListener = (e2: Error) => {
+      res.removeListener("data", chunkListener);
+      res.removeListener("end", endListener);
+      reject(e2);
+    };
+    const endListener = () => {
+      try {
+        res.removeListener("data", chunkListener);
+        res.removeListener("error", errorListener);
+        const contentType = res.headers["content-type"];
+
+        if (contentType && (contentType.indexOf("application/json") === 0 || contentType.indexOf("json") === 0)) {
+
+          data = JSON.parse(data);
+
+        }
+        const status = res.statusCode;
+        if (!status) {
+          const err = new ResponseError(status, res, res.headers, options.url, null, data);
+          reject(err);
+        } else {
+
+          if (status >= 300 && status <= 400 && !options.ignoreRedirect && res.headers["location"]) {
+            let location = res.headers["location"];
+            try {
+              const loURL = urlParse(location);
+              if (!loURL.hostname && url.hostname) {
+                // missing hostname on redirect so same hostname protocol and port?
+                loURL.hostname = url.hostname;
+                loURL.port = url.port;
+                loURL.path = url.path;
+                loURL.protocol = url.protocol;
+                location = formatUrl(loURL);
+              }
+
+              logger.debug(`redirecting to [${location}] from [${options.url}]`);
+              Util.request({
+                ...options,
+                url: location
+              }).then((ret) => {
+                const redirectedUrl = ret.url;
+                resolve({
+                  ...ret,
+                  url: options.url,
+                  redirectedUrl
+                });
+              }).catch((e4: any) => {
+                if (e4.response && e4.status && e4.headers && e4.data) {
+                  const err = new ResponseError(e4.status, e4.response, e4.headers, options.url, location, e4.data);
+                  err.stack = e4.stack;
+                  reject(err);
+                } else {
+                  (e4 as any).redirectedUrl = location;
+                  (e4 as any).url = options.url;
+                  reject(e4);
+                }
+              });
+            } catch (e5) {
+              reject(new ResponseError(status, res, res.headers, options.url, location, data));
+            }
+          } else {
+            if (status >= 200 && status < 300) {
+              resolve({
+                url: options.url,
+                response: res,
+                status,
+                redirectedUrl: null,
+                headers: res.headers,
+                request,
+                data
+              });
+            } else {
+              const err = new ResponseError(status, res, res.headers, options.url, null, data);
+              reject(err);
+            }
+          }
+        }
+      } catch (e) {
+        logger.error(e);
+        reject(e);
+      }
+    };
+
+    try {
+      res.on("data", chunkListener);
+      res.once("error", errorListener)
+      res.once("end", endListener);
+    } catch (e3) {
+      res.removeListener("data", chunkListener);
+      res.removeListener("end", endListener);
+      res.removeListener("error", errorListener);
+      reject(e3);
+    }
+  });
+
 }
 
 export const request = (options: RequestOptions, logger: Logger = Util.logger): Promise<RequestResponse> => {
@@ -88,96 +191,15 @@ export const request = (options: RequestOptions, logger: Logger = Util.logger): 
               timeout: options.timeout,
               hostname: url.hostname,
               port: url.port
-            }, (res) => {
-              let data: any = "";
-              const chunkListener = (chunk: any) => {
-                data += chunk;
-              };
-              const errorListener = (e2: Error) => {
-                res.removeListener("data", chunkListener);
-                res.removeListener("end", endListener);
-                reject(e2);
-              };
-              const endListener = () => {
-                res.removeListener("data", chunkListener);
-                res.removeListener("error", errorListener);
-                const contentType = res.headers["content-type"];
-
-                if (contentType && (contentType.indexOf("application/json") === 0 || contentType.indexOf("json") === 0)) {
-                  data = JSON.parse(data);
-                }
-                const status = res.statusCode;
-                if (!status) {
-                  const err = new ResponseError(status, res, res.headers, options.url, null, data);
-                  reject(err);
-                } else {
-
-                  if (status >= 300 && status <= 400 && !options.ignoreRedirect && res.headers["location"]) {
-                    let location = res.headers["location"];
-                    try {
-                      const loURL = urlParse(location);
-                      if (!loURL.hostname && url.hostname) {
-                        // missing hostname on redirect so same hostname protocol and port?
-                        loURL.hostname = url.hostname;
-                        loURL.port = url.port;
-                        loURL.path = url.path;
-                        loURL.protocol = url.protocol;
-                        location = formatUrl(loURL);
-                      }
-
-                      logger.debug(`redirecting to [${location}] from [${options.url}]`);
-                      Util.request({
-                        ...options,
-                        url: location
-                      }).then((ret) => {
-                        const redirectedUrl = ret.url;
-                        resolve({
-                          ...ret,
-                          url: options.url,
-                          redirectedUrl
-                        });
-                      }).catch((e4: any) => {
-                        if (e4.response && e4.status && e4.headers && e4.data) {
-                          const err = new ResponseError(e4.status, e4.response, e4.headers, options.url, location, e4.data);
-                          err.stack = e4.stack;
-                          reject(err);
-                        } else {
-                          (e4 as any).redirectedUrl = location;
-                          (e4 as any).url = options.url;
-                          reject(e4);
-                        }
-                      });
-                    } catch (e5) {
-                      reject(new ResponseError(status, res, res.headers, options.url, location, data));
-                    }
-                  } else {
-                    if (status >= 200 && status < 300) {
-                      resolve({
-                        url: options.url,
-                        response: res,
-                        status,
-                        redirectedUrl: null,
-                        headers: res.headers,
-                        request: req,
-                        data
-                      });
-                    } else {
-                      const err = new ResponseError(status, res, res.headers, options.url, null, data);
-                      reject(err);
-                    }
-                  }
-                }
-
-              }
+            }, (res: IncomingMessage) => {
               try {
-                res.on("data", chunkListener);
-                res.once("error", errorListener)
-                res.once("end", endListener);
-              } catch (e3) {
-                res.removeListener("data", chunkListener);
-                res.removeListener("end", endListener);
-                res.removeListener("error", errorListener);
-                reject(e3);
+                requestCallback(url, options, req, logger)(res).then((response) => {
+                  resolve(response);
+                }).catch((e) => {
+                  reject(e);
+                });
+              } catch (e) {
+                reject(e);
               }
             });
             req.once("error", (e: Error) => {
