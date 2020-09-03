645b3f37d9d2ffc20d4083ab27747c5e06f508ad
claukers <claukers@amelia>
2020-09-03 01:01:22 -0400

    fix FakeCallback type

--- src/util/test_helper.ts
+++ src/util/test_helper.ts
@@ -1,8 +1,8 @@
 export type callback<T = any> = (...args: any[]) => T;
 
-export interface FakeCallback extends callback {
+export interface FakeCallback<T = any> extends callback<T> {
   callCount: number;
-  callArgs: number;
+  callArgs: T[];
 }
 
 export const fake = (cb: callback): FakeCallback => {
